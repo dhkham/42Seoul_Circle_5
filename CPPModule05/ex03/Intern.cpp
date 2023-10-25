0efe3662249cbfda12e4d8e2011f322a27db97ce
Dongheon Kham <dkham@c4r5s3.42seoul.kr>
2023-10-25 18:31:11 +0900

    addd comment in Intern.cpp

--- CPPModule05/ex03/Intern.cpp
+++ CPPModule05/ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 19:58:14 by dkham             #+#    #+#             */
-/*   Updated: 2023/10/24 21:32:43 by dkham            ###   ########.fr       */
+/*   Updated: 2023/10/25 18:24:15 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 // Initialize static arrays
 const std::string Intern::formNames[] = {"shrubbery creation", "robotomy request", "presidential pardon"};
-const Intern::FormCreator Intern::formCreators[] = {&Intern::createShrubberyCreation, &Intern::createRobotomyRequest, &Intern::createPresidentialPardon};
+const Intern::FormCreator Intern::formCreators[] = {&Intern::createShrubberyCreation, &Intern::createRobotomyRequest, &Intern::createPresidentialPardon}; // Array of form creation functions: contains the addresses of the form creation functions
 
 Intern::Intern() {} // Default constructor
 
@@ -35,17 +35,27 @@ Intern::~Intern() {}
 
 AForm* Intern::makeForm(const std::string &formName, const std::string &target) {
     for (int i = 0; i < 3; ++i) {
-        if (formNames[i] == formName) {
+        if (formNames[i] == formName) { // if the form name passed as parameter exists (shrubbery creation, robotomy request, or presidential pardon)
             std::cout << "Intern creates " << formName << std::endl;
-            return (this->*formCreators[i])(target);  // Call the matching form creation function
+            return (this->*formCreators[i])(target);  // Call the matching function (target is passed as parameter)
         }
     }
     std::cout << "Error: Form " << formName << " not found!" << std::endl;
     return nullptr;
 }
+/*
+makeForm takes two strings:
+1. name of a form 2. target of the form.
+
+returns a pointer to a Form object (whose name is the one passed as parameter)
+whose target will be initialized to the second parameter.
+
+prints something like: Intern creates <form>
+If the form name passed as parameter doesn’t exist, print an explicit error message.
+*/
 
 AForm* Intern::createShrubberyCreation(const std::string &target) {
-    return new ShrubberyCreationForm(target);
+    return new ShrubberyCreationForm(target); // returns a pointer to a new ShrubberyCreationForm instance
 }
 
 AForm* Intern::createRobotomyRequest(const std::string &target) {
--- CPPModule05/ex03/main.cpp
+++ CPPModule05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 18:56:29 by dkham             #+#    #+#             */
-/*   Updated: 2023/10/24 21:33:18 by dkham            ###   ########.fr       */
+/*   Updated: 2023/10/25 18:24:36 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,11 @@ Intern 내부 parameterized constructor 확인
 */
 
 int main() {
-    Intern someRandomIntern;
+    Intern someRandomIntern; // create an instance of Intern
 
-    AForm* rrf;
-    rrf = someRandomIntern.makeForm("robotomy request", "Bender");
+    AForm* rrf; // rrf is a pointer to AForm
+    rrf = someRandomIntern.makeForm("robotomy request", "Bender"); // rrf points to a new RobotomyRequestForm instance
+    // makeForm method of Intern class calls createRobotomyRequest, which returns a pointer to a new RobotomyRequestForm instance.
     
     AForm* ppf;
     ppf = someRandomIntern.makeForm("presidential pardon", "Bender");
