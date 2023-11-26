25226c7ebceba12efae3d6e9cbfc3fbc31a1e6c3
Dongheon Kham <dkham@c4r6s6.42seoul.kr>
2023-11-26 18:54:45 +0900

    update functions

--- CPPModule07/ex01/iter.hpp
+++ CPPModule07/ex01/iter.hpp
@@ -6,15 +6,34 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 19:49:53 by dkham             #+#    #+#             */
-/*   Updated: 2023/11/20 20:58:08 by dkham            ###   ########.fr       */
+/*   Updated: 2023/11/26 18:09:32 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ITER_HPP
 #define ITER_HPP
 
+#include <cstddef>
+/*
+Non-const reference version (void (*func)(T&)): 
+used with functions that might modify the elements of the array. 
+suitable for scenarios where the elements of the array need to be changed.
+
+Const reference version (void (*func)(T const&)): 
+for functions that do not modify the elements of the array. 
+better choice for read-only operations on the array elements.
+*/
+
+// iter: iterates through an array and applies a function to each element
 template<typename T>
 void iter(T* array, size_t length, void (*func)(T&)) {
+    for (size_t i = 0; i < length; ++i) {
+        func(array[i]);
+    }
+}
+
+template<typename T>
+void iter(T* array, size_t length, void (*func)(T const&)) {
     for (size_t i = 0; i < length; ++i) {
         func(array[i]);
     }
--- CPPModule07/ex01/main.cpp
+++ CPPModule07/ex01/main.cpp
@@ -6,38 +6,36 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 19:49:29 by dkham             #+#    #+#             */
-/*   Updated: 2023/11/20 21:01:11 by dkham            ###   ########.fr       */
+/*   Updated: 2023/11/26 18:54:28 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
 #include "iter.hpp"
+#include <iostream>
+#include <string>
 
-// Example function to be used with iter
-template<typename T>
-void printElement(T& elem) {
-    std::cout << elem << std::endl;
+void printElement(int const& element) {
+    std::cout << element << " ";
 }
 
-template<typename T>
-void addOne(T& elem) {
-    elem += 1;
+void incrementElement(int& element) {
+    element++;
 }
 
-// const 처리???
 int main() {
     int intArray[] = {1, 2, 3, 4, 5};
-    std::string strArray[] = {"Hello", "World", "C++", "Template"};
+    size_t intArraySize = sizeof(intArray) / sizeof(intArray[0]);
 
-    std::cout << "Integer Array:" << std::endl;
-    iter(intArray, 5, printElement);
-    
-    std::cout << "Integer Array + 1:" << std::endl;
-    iter(intArray, 5, addOne);
-    iter(intArray, 5, printElement);
+    // Using iter with a function that does not modify the array
+    iter(intArray, intArraySize, printElement);
+    std::cout << std::endl;
 
-    std::cout << "String Array:" << std::endl;
-    iter(strArray, 4, printElement);
-    
+    // Using iter with a function that modifies the array
+    iter(intArray, intArraySize, incrementElement);
+
+    // Printing the modified array
+    iter(intArray, intArraySize, printElement);
+    std::cout << std::endl;
+
     return 0;
 }
