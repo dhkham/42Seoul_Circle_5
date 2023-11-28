c8ddd1384d93b2b7baaf21b79d39503d2fc73fad
Dongheon Kham <dkham@c4r5s7.42seoul.kr>
2023-11-28 20:40:48 +0900

    fix errors

--- CPPModule07/ex01/main.cpp
+++ CPPModule07/ex01/main.cpp
@@ -6,42 +6,62 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 19:49:29 by dkham             #+#    #+#             */
-/*   Updated: 2023/11/26 19:02:30 by dkham            ###   ########.fr       */
+/*   Updated: 2023/11/28 20:36:08 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iter.hpp"
-#include <iostream>
-#include <string>
 
-/*
-    printElement, incrementElement: 템플릿으로 안 써도 되는 것인지 확인
+int main(void)
+{
+    // non const version
+    int intArr[] = {1, 2, 3};
+    char strArr[] = {'a', 'b', 'c'};
+    size_t intArrLength = sizeof(intArr) / sizeof(intArr[0]);
+    size_t strArrLength = sizeof(strArr) / sizeof(strArr[0]);
+
+    std::cout << "<<< BEFORE >>>" << std::endl << std::endl;
     
-    Your iter function template must work with any type of array. The third parameter can be an instantiated function template.
-    -> 이 조건 만족하는지 확인
-*/
-void printElement(int const& element) {
-    std::cout << element << " ";
-}
-
-void incrementElement(int& element) {
-    element++;
-}
-
-int main() {
-    int intArray[] = {1, 2, 3, 4, 5};
-    size_t intArraySize = sizeof(intArray) / sizeof(intArray[0]);
-
-    // Using iter with a function that does not modify the array
-    iter(intArray, intArraySize, printElement);
+    // apply printArray
+    std::cout << "intArr:" << std::endl;
+    iter(intArr, intArrLength, printArray);
     std::cout << std::endl;
 
-    // Using iter with a function that modifies the array
-    iter(intArray, intArraySize, incrementElement);
+    std::cout << "strArr:" << std::endl;
+    iter(strArr, strArrLength, printArray);
+    std::cout << std::endl;
 
-    // Printing the modified array
-    iter(intArray, intArraySize, printElement);
+    // apply incrementArray
+    iter(intArr, intArrLength, incrementArray);
+    iter(strArr, strArrLength, incrementArray);
+    std::cout << std::endl;
+
+    std::cout << "<<< AFTER >>>" << std::endl << std::endl;
+    
+    // apply printArray
+    std::cout << "intArr:" << std::endl;
+    iter(intArr, intArrLength, printArray);
+    std::cout << std::endl;
+
+    std::cout << "char_array:" << std::endl;
+    iter(strArr, strArrLength, printArray);
+    std::cout << std::endl;
+
+    // const version
+    std::cout << "*************************************************************" << std::endl << std::endl;
+    const int const_intArr[] = {1, 2, 3};
+    const char const_strArr[] = {'a', 'b', 'c'};
+    size_t const_intArrLength = sizeof(const_intArr) / sizeof(const_intArr[0]);
+    size_t const_strArrLength = sizeof(const_strArr) / sizeof(const_strArr[0]);
+        
+    // apply printArray
+    std::cout << "const_intArr:" << std::endl;
+    iter(const_intArr, const_intArrLength, printArray);
     std::cout << std::endl;
     
+    std::cout << "const_strArr:" << std::endl;
+    iter(const_strArr, const_strArrLength, printArray);
+    std::cout << std::endl;
+
     return 0;
 }
