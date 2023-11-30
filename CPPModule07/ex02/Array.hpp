067317c51a40f54887f14ce2a68e2a68760a3721
Dongheon Kham <dkham@c4r6s6.42seoul.kr>
2023-11-30 21:27:28 +0900

    update code

--- CPPModule07/ex02/Array.hpp
+++ CPPModule07/ex02/Array.hpp
@@ -6,36 +6,36 @@
 template <typename T>
 class Array {
 public:
-    Array();
-    Array(unsigned int n);
-    Array(const Array& other);
-    Array& operator=(const Array& other);
-    ~Array();
+    Array(); // default constructor: creates an empty array
+    Array(unsigned int n); // constructor with parameter: creates an array of size n, initialized with default values
+    Array(const Array& other); // copy constructor
+    Array& operator=(const Array& other); // assignment operator
+    ~Array(); // destructor
 
-    T& operator[](unsigned int index);
-    const T& operator[](unsigned int index) const;
-    unsigned int size() const;
+    T& operator[](unsigned int index); // access element
+    const T& operator[](unsigned int index) const; // access element
+    unsigned int size() const; // return size of the array
 
 private:
-    T* _array;
-    unsigned int _size;
+    T* _array;  // pointer to the first element of the array
+    unsigned int _size; // number of elements in the array
 };
 
 // Implementation
 
 template<typename T>
-Array<T>::Array() : _array(nullptr), _size(0) {}
+Array<T>::Array() : _array(NULL), _size(0) {} // construction with no parameter: creates an empty array
 
 template<typename T>
-Array<T>::Array(unsigned int n) : _array(new T[n]()), _size(n) {}
+Array<T>::Array(unsigned int n) : _array(new T[n]()), _size(n) {} // construction with parameter: creates an array of size n, initialized with default values
 
 template<typename T>
-Array<T>::Array(const Array<T>& other) : _array(nullptr), _size(0) {
+Array<T>::Array(const Array<T>& other) : _array(NULL), _size(0) { // copy constructor
     *this = other;
 }
 
 template<typename T>
-Array<T>& Array<T>::operator=(const Array<T>& other) {
+Array<T>& Array<T>::operator=(const Array<T>& other) { // assignment operator
     if (this != &other) {
         delete[] _array;
         _array = new T[other._size]();
@@ -48,26 +48,26 @@ Array<T>& Array<T>::operator=(const Array<T>& other) {
 }
 
 template<typename T>
-Array<T>::~Array() {
+Array<T>::~Array() { // destructor
     delete[] _array;
 }
 
 template<typename T>
-T& Array<T>::operator[](unsigned int index) {
+T& Array<T>::operator[](unsigned int index) { // access element
     if (index >= _size)
         throw std::out_of_range("Index out of bounds");
     return _array[index];
 }
 
 template<typename T>
-const T& Array<T>::operator[](unsigned int index) const {
+const T& Array<T>::operator[](unsigned int index) const { // access element
     if (index >= _size)
         throw std::out_of_range("Index out of bounds");
     return _array[index];
 }
 
 template<typename T>
-unsigned int Array<T>::size() const {
+unsigned int Array<T>::size() const { // return size of the array
     return _size;
 }
 
--- CPPModule07/ex02/main.cpp
+++ CPPModule07/ex02/main.cpp
@@ -3,22 +3,44 @@
 
 int main() {
     try {
-        Array<int> intArray(5);
+        Array<int> intArray(5); // Array of 5 integers
 
-        for (unsigned int i = 0; i < intArray.size(); ++i) {
-            intArray[i] = i * 10;
+        std::cout << "original array: " << std::endl;
+        for (size_t i = 0; i < intArray.size(); ++i) { // Fill the array with values
+            intArray[i] = i * 10; // Set the value at index i
             std::cout << intArray[i] << " ";
         }
         std::cout << std::endl;
 
         // Copy test
-        Array<int> copyArray(intArray);
-        copyArray[0] = 100;
+        Array<int> copyArray(intArray); // Copy constructor
+        copyArray[0] = 10; // Change the first element of the copy
 
-        std::cout << "Original: " << intArray[0] << ", Copy: " << copyArray[0] << std::endl;
+        std::cout << "copy array: " << std::endl;
+        for (size_t i = 0; i < copyArray.size(); ++i) { // Fill the array with values
+            std::cout << copyArray[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // Assignment test
+        Array<int> assignArray = intArray; // Assignment operator
+        assignArray[0] = 100; // Change the first element of the copy
+
+        std::cout << "assignment array: " << std::endl;
+        for (size_t i = 0; i < assignArray.size(); ++i) { // Fill the array with values
+            std::cout << assignArray[i] << " ";
+        }
+        std::cout << std::endl;
+
+        // original array again
+        std::cout << "original array: " << std::endl;
+        for (size_t i = 0; i < intArray.size(); ++i) { // Fill the array with values
+            std::cout << intArray[i] << " ";
+        }
+        std::cout << std::endl;
 
         // Out of bounds test
-        std::cout << intArray[6] << std::endl;
+        std::cout << intArray[6] << std::endl; // Should throw an exception
     }
     catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
