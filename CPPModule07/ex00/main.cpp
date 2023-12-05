fab20016500f1736aae84c576a6f909f6a9ce62a
Dongheon Kham <dkham@c4r6s1.42seoul.kr>
2023-12-05 19:47:24 +0900

    remove :: sign from main

--- CPPModule07/ex00/main.cpp
+++ CPPModule07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 18:51:47 by dkham             #+#    #+#             */
-/*   Updated: 2023/11/20 19:49:02 by dkham            ###   ########.fr       */
+/*   Updated: 2023/12/05 19:46:49 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ int main() {
     int a = 2;
     int b = 3;
     
-    ::swap(a, b); // :: indicates that the function is in the global namespace (= namespace that encompasses all identifiers (like variables, functions, classes, etc.) that are not defined within a user-defined namespace)
+    swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
     std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
     std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
 
-    std::string c = "chaine1";
-    std::string d = "chaine2";
-    ::swap(c, d);
+    std::string c = "str1";
+    std::string d = "str2";
+    swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
     std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
     std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
--- CPPModule07/ex00/whatever.hpp
+++ CPPModule07/ex00/whatever.hpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Whatever.hpp                                       :+:      :+:    :+:   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 18:57:40 by dkham             #+#    #+#             */
-/*   Updated: 2023/11/20 19:44:38 by dkham            ###   ########.fr       */
+/*   Updated: 2023/12/05 19:35:29 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,22 @@
 #include <iostream>
 #include <string>
 
-// Swaps the values of two given arguments. Does not return anything.
+// Swaps the values of two given arguments.
+// template function: a function that can operate with generic types
 template<typename T>
-void swap(T& a, T& b) {
+void swap(T& a, T& b) { // takes two references to objects as parameters
     T temp = a;
     a = b;
     b = temp;
 }
 
-// Returns the smallest one. If the two of them are equal, then it returns the second one
+// Returns the smallest one. (If the two of them are equal, then it returns the second one)
 template<typename T>
 T const & min(T const & a, T const & b) { // takes two references to const objects as parameters (const: the value of the object cannot be modified, &: use of references to avoid copying the objects)
     return (b <= a) ? b : a;
 }
 
-// Returns the greatest one. If the two of them are equal, then it returns the second one
+// Returns the greatest one. (If the two of them are equal, then it returns the second one)
 template<typename T>
 T const & max(T const & a, T const & b) {
     return (a <= b) ? b : a;
