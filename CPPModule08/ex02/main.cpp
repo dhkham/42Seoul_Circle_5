d5fdd6f8198e790bfd48f58865ffd15392ab3804
Dongheon Kham <dkham@c4r5s7.42seoul.kr>
2023-12-20 19:00:16 +0900

    add more tests to main.cpp

--- CPPModule08/ex02/main.cpp
+++ CPPModule08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 20:42:07 by dkham             #+#    #+#             */
-/*   Updated: 2023/12/13 20:42:08 by dkham            ###   ########.fr       */
+/*   Updated: 2023/12/20 18:59:56 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,32 @@ int main() {
         ++it;
     }
 
-    std::stack<int> s(mstack); // Copying the MutantStack to a standard stack (copy constructor)
+    std::stack<int> s(mstack);
+    
+    //test if copy constructor works
+    std::cout << "test if copy constructor works: " << std::endl;
+    
+    MutantStack<int> mstack2(mstack); //copy constructor
+    MutantStack<int>::iterator it2 = mstack2.begin();
+    MutantStack<int>::iterator ite2 = mstack2.end();
+
+    while (it2 != ite2) {
+        std::cout << *it2 << std::endl;
+        ++it2;
+    }
+    
+    // test if assignation operator works
+    std::cout << "test if assignation operator works: " << std::endl;
+    
+    MutantStack<int> mstack3;
+    mstack3 = mstack; //assignation operator
+    MutantStack<int>::iterator it3 = mstack3.begin();
+    MutantStack<int>::iterator ite3 = mstack3.end();
+    
+    while (it3 != ite3) {
+        std::cout << *it3 << std::endl;
+        ++it3;
+    }
     
     // std::list<int> lst;
     // lst.push_back(5);
