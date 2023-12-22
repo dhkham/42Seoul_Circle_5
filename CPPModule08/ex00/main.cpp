a71ef10f12fc4e419a4c5d3686ead241a252e7f5
Dongheon Kham <dkham@c4r5s7.42seoul.kr>
2023-12-22 13:19:28 +0900

    add comment for iterator

--- CPPModule08/ex00/main.cpp
+++ CPPModule08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dkham <dkham@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 20:55:20 by dkham             #+#    #+#             */
-/*   Updated: 2023/12/13 20:55:20 by dkham            ###   ########.fr       */
+/*   Updated: 2023/12/22 13:17:48 by dkham            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,7 @@ int main() {
     v.push_back(5);
 
     try {
+        // iterator: object that facilitates the traversal of a container
         std::vector<int>::iterator it = easyfind(v, 3); // declaring an iterator it for a vector of integers and initializing it to the position of the element 3 in the vector v, as found by the easyfind function.
         std::cout << "Found: " << *it << std::endl;     // Print the found element
     } catch (const std::exception& e) {
